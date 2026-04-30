11262db3b528dbe74467c0ef1dc58f30c72cc378
Sadikulw <sadikulislam26064@gmail.com>
2026-04-30 18:36:37 +0530

    fix navbar ui

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -3,28 +3,22 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav
-      className=" navbar navbar-expand-lg border-bottom "
+      className=" navbar navbar-expand-lg border-bottom  "
       style={{ backgroundColor: "#FFF" }}
     >
-      <div className="container p-2">
-        <Link className="navbar-brand" to="/">
-          <img
-            src="/media/image/logo.svg"
-            style={{ width: "145px" }}
-            alt="Logo"
-          />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <div className="container p-2 margin">
+        <div>
+          <Link className="navbar-brand" to="/">
+            <img
+              src="/media/image/logo.svg"
+              style={{ width: "145px" }}
+              alt="Logo"
+            />
+          </Link>
+        </div>
+<div>
+  
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <form className="d-flex" role="search">
             <ul className="navbar-nav mb-lg-0 gap-4">
@@ -63,6 +57,7 @@ function Navbar() {
               </li>
             </ul>
           </form>
+        </div>
         </div>
       </div>
     </nav>
